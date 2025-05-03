6b53cc24638d9fc40ef971a993c0a125226df917
Aadil Majeed <108990328+Adilmajeed58@users.noreply.github.com>
2025-05-03 11:39:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -496,4 +496,16 @@ document.addEventListener('DOMContentLoaded', () => {
       console.log('Record Deleted at Index:', index);
     }
   };
+   //serviceworker
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js')
+        .then(registration => {
+          console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch(error => {
+          console.error('Service Worker registration failed:', error);
+        });
+    });
+  }
 });
